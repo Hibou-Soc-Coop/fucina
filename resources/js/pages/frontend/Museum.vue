786a64d6ce3b532993745f8fb18a99c61e82770d
LRiccio <leo.riccio@hiboucoop.org>
2025-12-29 17:00:13 +0100

    refactor: migliora la struttura del componente Museum, aggiungi scroll e ottimizza il caricamento

--- resources/js/pages/frontend/Museum.vue
+++ resources/js/pages/frontend/Museum.vue
@@ -1,46 +1,53 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import LogoLight from '@storage/assets/logo.svg';
+import { router, usePage } from '@inertiajs/vue3';
 import Dropdown from '@storage/assets/drop-down.svg';
-import { router } from '@inertiajs/vue3';
-import { usePage } from '@inertiajs/vue3';
+import LogoLight from '@storage/assets/logo.svg';
+import { ref } from 'vue';
 import { useI18n } from 'vue-i18n';
 
 const page = usePage();
 const languages = page.props.languages as Array<{ code: string; name: string }>;
-const { t , locale } = useI18n();
+const { t, locale } = useI18n();
 
 const props = defineProps<{ museumId: number; language: string }>();
-
-
 
 const currentLanguage = ref(props.language);
 
 const loading = ref(true);
+const containerRef = ref<HTMLElement | null>(null);
+
+const scrollToNextScreen = () => {
+    containerRef.value?.scrollBy({ top: window.innerHeight, behavior: 'smooth' });
+};
 
 setTimeout(() => {
     loading.value = false;
 }, 2000);
-
 </script>
 
 <template>
-    <div class="relative h-screen w-screen snap-center snap-y snap-mandatory overflow-y-scroll">
-        <transition leave-active-class="transition-opacity duration-3000" leave-from-class="opacity-0"
-            leave-to-class="opacity-100">
-            <div v-if="!loading" class="h-full flex justify-center items-center ">
-                <img class="absolute inset-0 h-full" src="@storage/assets/2.jpeg" alt="">
-                <img class="relative z-10 w-[187px] h-[454px]" src="@storage/assets/logo-trasparente.png" alt="">
-                <Dropdown class="absolute bottom-8 place-self-center not-last:animate-[bounce_2s_infinite]" />
+    <div ref="containerRef" class="relative h-screen w-screen snap-y snap-mandatory overflow-y-scroll">
+        <transition leave-active-class="transition-opacity duration-3000" leave-from-class="opacity-0" leave-to-class="opacity-100">
+            <div v-if="!loading" class="flex h-full w-full snap-start items-center justify-center">
+                <img class="absolute inset-0 h-full" src="@storage/assets/2.jpeg" alt="" />
+                <img class="relative z-10 h-[454px] w-[187px]" src="@storage/assets/logo-trasparente.png" alt="" />
+                <Dropdown
+                    class="absolute bottom-8 cursor-pointer place-self-center not-last:animate-[bounce_2s_infinite]"
+                    @click="scrollToNextScreen"
+                />
             </div>
         </transition>
-        <div v-if="!loading" class="intro h-full bg-white py-4">
+        <div v-if="!loading" class="intro h-full w-full snap-start bg-white p-4">
             <p class="text-lg font-medium text-black">{{ t('intro.Introduction') }}</p>
-            <button  class="bg-black block text-white font-bold mx-auto py-4 px-8 mt-8" @click="router.visit(`/museum/${props.museumId}/collections/1`)">ENTRA</button>
+            <button
+                class="mx-auto mt-8 block bg-black px-8 py-4 font-bold text-white"
+                @click="router.visit(`/museum/${props.museumId}/collections/1`)"
+            >
+                ENTRA
+            </button>
         </div>
-        <transition leave-active-class="transition-opacity duration-3000" leave-from-class="opacity-100"
-            leave-to-class="opacity-0">
-            <div v-if="loading" class="absolute inset-0 flex justify-center bg-black items-center ">
+        <transition leave-active-class="transition-opacity duration-3000" leave-from-class="opacity-100" leave-to-class="opacity-0">
+            <div v-if="loading" class="absolute inset-0 flex items-center justify-center bg-black">
                 <LogoLight />
             </div>
         </transition>
